343c100a4b6389218e7d92b226b1e991a749188d
allgrego <allgrego14@gmail.com>
2022-10-08 23:23:39 -0400

    [fix] Change local API url in fetching

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -1,6 +1,5 @@
-
 export const getRandomActivity = async () => {
-    const url = 'http://localhost:3000/api/activities/random'
+    const url = '/api/activities/random'
     const response = await fetch(url)
     return await response.json()
 }
